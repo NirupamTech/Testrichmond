0dc0fdccd6c651f92cb2f9b9090dd36b52b99d5f
Nirupam <72504541+NirupamTech@users.noreply.github.com>
2023-10-21 21:23:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,47 @@
-const stripe = Stripe('YOUR_PUBLISHABLE_KEY'); // Replace with your actual publishable key
-const elements = stripe.elements();
-const cardElement = elements.create('card');
+function generateTimetable() {
+  const subject = document.getElementById('subject').value;
+  const difficulty = document.getElementById('difficulty').value;
+  const studyTime = parseInt(document.getElementById('studyTime').value);
 
-cardElement.mount('#card-element');
+  const formulasContent = getFormulasBySubject(subject);
 
-const form = document.getElementById('payment-form');
-const cardErrors = document.getElementById('card-errors');
-const successMessage = document.getElementById('success-message');
+  const content = `
+    Subject: ${subject}
+    Difficulty: ${difficulty}
+    Total Study Time: ${studyTime} minutes
 
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  form.querySelector('button').disabled = true;
+    Important Topics:
+    ${getTopicsBySubject(subject)}
 
-  // Create a payment intent or token on the server
-  // You should handle this part on your server
-  // For simplicity, you can create a fixed amount intent for $1
+    Formulas:
+    ${formulasContent}
+  `;
 
-  const { paymentMethod, error } = await stripe.confirmCardPayment('CLIENT_SECRET', {
-    payment_method: {
-      card: cardElement,
-    },
-  });
+  const printWindow = window.open('', '_blank');
+  printWindow.document.write('<html><head><title>Study Timetable</title></head><body>');
+  printWindow.document.write('<pre>' + content + '</pre>');
+  printWindow.document.write('</body></html>');
+  printWindow.document.close();
 
-  if (error) {
-    cardErrors.textContent = error.message;
-    cardErrors.style.display = 'block';
-    form.querySelector('button').disabled = false;
-  } else {
-    // Payment successful
-    successMessage.style.display = 'block';
+  // Print the window
+  printWindow.print();
+}
 
-    // Proceed to generate the timetable
-
-    const subject = document.getElementById('subject').value;
-    const difficulty = document.getElementById('difficulty').value;
-    const studyTime = parseInt(document.getElementById('studyTime').value);
-
-    // Fetch important topics based on the selected subject
-    const topics = await fetchTopicsBySubject(subject);
-
-    // Fetch formulas based on the selected subject
-    const formulasContent = await fetchFormulasBySubject(subject);
-
-    // Create the content for the timetable
-    const content = `
-      Subject: ${subject}
-      Difficulty: ${difficulty}
-      Total Study Time: ${studyTime} minutes
-
-      Important Topics:
-      ${topics}
-
-      Formulas:
-      ${formulasContent}
-    `;
-
-    // Create a new window to print the content
-    const printWindow = window.open('', '_blank');
-    printWindow.document.write('<html><head><title>Study Timetable</title></head><body>');
-    printWindow.document.write('<pre>' + content + '</pre>');
-    printWindow.document.write('</body></html>');
-    printWindow.document.close();
-
-    // Print the window
-    printWindow.print();
-  }
-});
-
-// Function to fetch important topics based on subject
-async function fetchTopicsBySubject(subject) {
-  // Implement your logic to fetch topics here, e.g., via an API
-  // Return the fetched topics as a string
+// Function to get topics based on subject
+function getTopicsBySubject(subject) {
+  // Add logic to get topics based on subject
+  // This is just a placeholder
   return 'Topics for this subject:\nTopic 1: ...\nTopic 2: ...';
 }
 
-// Function to fetch formulas based on subject
-async function fetchFormulasBySubject(subject) {
-  // Implement your logic to fetch formulas here, e.g., via an API
-  // Return the fetched formulas as a string
-  return 'Formulas for this subject:\nFormula 1: ...\nFormula 2: ...';
-                                                         }
+// Simulate a $1 payment
+function processPayment() {
+  const paymentAmount = 1; // $1
+
+  // Perform any necessary payment processing logic here
+
+  // Display a success message
+  alert(`Payment of $${paymentAmount} successful!`);
+
+  // You can also trigger other actions or update your application's state here
+}
