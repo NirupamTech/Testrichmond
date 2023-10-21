2e663e547814ba53c7b3fd5e57198f1cb887f320
Nirupam <72504541+NirupamTech@users.noreply.github.com>
2023-10-21 16:31:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,82 @@
+const stripe = Stripe('YOUR_PUBLISHABLE_KEY'); // Replace with your actual publishable key
+const elements = stripe.elements();
+const cardElement = elements.create('card');
+
+cardElement.mount('#card-element');
+
+const form = document.getElementById('payment-form');
+const cardErrors = document.getElementById('card-errors');
+const successMessage = document.getElementById('success-message');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  form.querySelector('button').disabled = true;
+
+  // Create a payment intent or token on the server
+  // You should handle this part on your server
+  // For simplicity, you can create a fixed amount intent for $1
+
+  const { paymentMethod, error } = await stripe.confirmCardPayment('CLIENT_SECRET', {
+    payment_method: {
+      card: cardElement,
+    },
+  });
+
+  if (error) {
+    cardErrors.textContent = error.message;
+    cardErrors.style.display = 'block';
+    form.querySelector('button').disabled = false;
+  } else {
+    // Payment successful
+    successMessage.style display = 'block';
+
+    // Proceed to generate the timetable
+
+    const subject = document.getElementById('subject').value;
+    const difficulty = document.getElementById('difficulty').value;
+    const studyTime = parseInt(document.getElementById('studyTime').value);
+
+    // Fetch important topics based on the selected subject
+    const topics = await fetchTopicsBySubject(subject);
+
+    // Fetch formulas based on the selected subject
+    const formulasContent = await fetchFormulasBySubject(subject);
+
+    // Create the content for the timetable
+    const content = `
+      Subject: ${subject}
+      Difficulty: ${difficulty}
+      Total Study Time: ${studyTime} minutes
+
+      Important Topics:
+      ${topics}
+
+      Formulas:
+      ${formulasContent}
+    `;
+
+    // Create a new window to print the content
+    const printWindow = window.open('', '_blank');
+    printWindow.document.write('<html><head><title>Study Timetable</title></head><body>');
+    printWindow.document.write('<pre>' + content + '</pre>');
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+
+    // Print the window
+    printWindow.print();
+  }
+});
+
+// Function to fetch important topics based on subject
+async function fetchTopicsBySubject(subject) {
+  // Implement your logic to fetch topics here, e.g., via an API
+  // Return the fetched topics as a string
+  return 'Topics for this subject:\nTopic 1: ...\nTopic 2: ...';
+}
+
+// Function to fetch formulas based on subject
+async function fetchFormulasBySubject(subject) {
+  // Implement your logic to fetch formulas here, e.g., via an API
+  // Return the fetched formulas as a string
+  return 'Formulas for this subject:\nFormula 1: ...\nFormula 2: ...';
+                                                         }
