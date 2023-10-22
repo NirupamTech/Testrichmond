8105b5b9d4e6b2d377f2e0ca9ab107e76283ffcd
Nirupam <72504541+NirupamTech@users.noreply.github.com>
2023-10-22 12:01:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-// In your JavaScript code (script.js)
 function generateTimetable() {
   const subject = document.getElementById('subject').value;
   const difficulty = document.getElementById('difficulty').value;
   const studyTime = parseInt(document.getElementById('studyTime').value);
 
-  // Set the payment amount to $1 (100 cents)
-  const paymentAmount = 100; // $1 = 100 cents
+  const formulasContent = getFormulasBySubject(subject);
 
-  // Collect the user's input and any other necessary data.
-  const paymentData = {
-    subject,
-    difficulty,
-    studyTime,
-    amount: paymentAmount, // Include the payment amount in your data.
-    // Other payment-related data.
+  const content = `
+    Subject: ${subject}
+    Difficulty: ${difficulty}
+    Total Study Time: ${studyTime} minutes
+
+    Important Topics:
+    ${getTopicsBySubject(subject)}
+
+    Formulas:
+    ${formulasContent}
   };
 
-  // Send a request to your server to initiate the payment via PayU.
-  fetch('/initiate-payu-payment', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      // Add PayU API client ID and secret keys as headers
-      'PayU-Client-Id': 'e19bef6306905ea7e70d9ae79b80bad3ce8196f03f843ed5c44fd0d323126ad6',
-      'PayU-Client-Secret': '469876ed7bf4282193d12cadeaae0df04025821dc3c8d48f6e2f2f8e16b8166c',
-    },
-    body: JSON.stringify(paymentData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // Handle the payment response from your server.
-      console.log(data);
-      // You may need to redirect the user to PayU's payment page.
-    })
-    .catch((error) => {
-      console.error('Payment error:', error);
-    });
+  const printWindow = window.open('', '_blank');
+  printWindow.document.write('<html><head><title>Study Timetable</title></head><body>');
+  printWindow.document.write('<pre>' + content + '</pre>');
+  printWindow.document.write('</body></html>');
+  printWindow.document.close();
+
+  // Print the window
+  printWindow.print();
 }
 
 // Function to get topics based on subject
